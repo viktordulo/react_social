c3feff4c994610a6ec920b1791448f754f9b916d
viktordulo <victordulo96@gmail.com>
2020-12-03 22:44:39 +0200

    added components to messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,34 +1,36 @@
+import { NavLink } from 'react-router-dom'
 import s from './Messages.module.css'
+
+const User = (props) => {
+    return (
+        <NavLink className={s.user} activeClassName={s.active} to={'/messages/' + props.id}>
+            <img src={props.img} alt='ava' />
+            <p>{props.name}</p>
+        </NavLink>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.send}</div>
+    )
+}
+
 const Messages = (props) => {
     return (
         <div className={s.chats}>
             <div className={s.people}>
-                <div className={s.user}>
-                    <img src="https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt='ava' />
-                    <p>Kate</p>
-                </div>
-                <div className={s.user}>
-                    <img src="https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt='ava' />
-                    <p>Nastya</p>
-                </div>
-                <div className={s.user}>
-                    <img src="https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt='ava' />
-                    <p>Vika</p>
-                </div>
-                <div className={s.user}>
-                    <img src="https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt='ava' />
-                    <p>Aleksei</p>
-                </div>
-                <div className={s.user}>
-                    <img src="https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt='ava' />
-                    <p>Dima</p>
-                </div>
+                <User img='https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg' name='Kate' id='1' />
+                <User img='https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg' name='Nastya' id='2' />
+                <User img='https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg' name='Vika' id='3' />
+                <User img='https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg' name='Aleksei' id='4' />
+                <User img='https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14044.jpg' name='Dima' id='5' />
             </div>
             <div className={s.dialog}>
-                <div className={s.message}>Zdarova, I'm from Irkutsk</div>
-                <div className={s.message}>Who're you?</div>
-                <div className={s.message}>I'm arbuzniy chelovek</div>
-                <div className={s.message}>You're so strange man. Don't write me more</div>
+                <Message send="Zdarova, I'm from Irkutsk"/>
+                <Message send="Who're you?"/>
+                <Message send="I'm arbuzniy chelovek"/>
+                <Message send="You're so strange man. Don't write me more"/>
             </div>
         </div>
     )
